c56f6b4d6ef18c050ead6fbb2d0a65d1d2a28925
Andreas Steiner <andreas.steiner@unibas.ch>
2014-01-17 18:31:12 +0000

    updated push_docs.sh

--- push_docs.sh
+++ push_docs.sh
@@ -11,7 +11,7 @@ fi
 (cd docs && make html)
 rm -rf $TMP/docs
 mkdir -p $TMP/docs
-cp -r docs/_buil/html $TMP/docs
+cp -r docs/_build/html $TMP/docs
 git checkout gh-pages
 git add .
 git commit -m'auto-update gh-pages branch'
