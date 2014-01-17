9b41120e7fcd0404d5253b7f31526750f3018a18
Andreas Steiner <andreas.steiner@unibas.ch>
2014-01-17 18:28:16 +0000

    added clenliness check

--- push_docs.sh
+++ push_docs.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-
 cd `dirname $0`
+
+if [[ -n $(git status -s) ]]; then
+    echo -e "\033[91m *** directory not clean -- aborting *** \033[m"
+    exit 1
+fi
+
 (cd docs && make html)
 rm -rf tmp
 cp -r docs/_buil/html tmp
