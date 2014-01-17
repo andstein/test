75c37701de2ba6610b3d35c058e5fe32195daba9
Andreas Steiner <andreas.steiner@unibas.ch>
2014-01-17 18:52:15 +0000

    updated push_docs.sh

--- push_docs.sh
+++ push_docs.sh
@@ -3,6 +3,7 @@
 TMP=tmp
 cd `dirname $0`
 
+# make sure we're clean
 if [[ -n $(git status -s) ]]; then
     echo -e "\033[91m *** directory not clean -- aborting *** \033[m"
     exit 1
@@ -10,10 +11,12 @@ fi
 
 git checkout gh-pages
 git checkout master -- docs/_build/html
+# do not add any leftovers from master branch
 git checkout master -- .gitignore
 cp -r docs/_build/html/* .
 rm -rf docs
 git add --all .
 git commit -m'auto-updated gh-pages from master'
+git push
 git checkout master
 
